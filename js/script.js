e3de8e7e0e743d2908b9cea6d0bb48de3f717f26
Kaustubh Singh <kaustubh5410@gmail.com>
2021-08-04 01:40:45 +0530

    commented js code for auto collapse that was causing unexpected behaviour on clicking a navbar link

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
-$(function () { // Same as document.addEventListener("DOMContentLoaded"...
+// $(function () { // Same as document.addEventListener("DOMContentLoaded"...
 
-    // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
-    $(".navbar-toggler").blur(function (event) {
-        var screenWidth = window.innerWidth;
-        if (screenWidth < 992) {
-            $(".navbar-collapse").collapse('hide');
-            $('#navbar-button').children('span').removeClass('fa fa-times fa-lg');
-            $('#navbar-button').children('span').addClass('navbar-toggler-icon');
-        }
-    });
+//     // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
+//     $(".navbar-toggler").blur(function (event) {
+//         var screenWidth = window.innerWidth;
+//         if (screenWidth < 992) {
+//             $(".navbar-collapse").collapse('hide');
+//             $('#navbar-button').children('span').removeClass('fa fa-times fa-lg');
+//             $('#navbar-button').children('span').addClass('navbar-toggler-icon');
+//         }
+//     });
 
-    // In Firefox and Safari, the click event doesn't retain the focus
-    // on the clicked button. Therefore, the blur event will not fire on
-    // user clicking somewhere else in the page and the blur event handler
-    // which is set up above will not be called.
-    // Solution: force focus on the element that the click event fired on
-    $(".navbar-toggler").click(function (event) {
-        $(event.target).focus();
-    });
-});
+//     // In Firefox and Safari, the click event doesn't retain the focus
+//     // on the clicked button. Therefore, the blur event will not fire on
+//     // user clicking somewhere else in the page and the blur event handler
+//     // which is set up above will not be called.
+//     // Solution: force focus on the element that the click event fired on
+//     $(".navbar-toggler").click(function (event) {
+//         $(event.target).focus();
+//     });
+// });
 
 
 $(document).ready(function (){
